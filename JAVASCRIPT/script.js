9896877c3eb26baaea3deae8f8537793402caf5a
ATOROYO <davidatoroyosika@gmail.com>
2024-09-20 17:01:27 +0300

    Style the sticky nav-bar using JavaScript

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".nav-bar").addClass("sticky");
+    } else {
+      $(".nav-bar").removeClass("sticky");
+    }
+    if (this.scroll > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+});
