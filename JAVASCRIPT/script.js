bb5b30625a1c233c4f98629b4dab909105e429c8
ATOROYO <davidatoroyosika@gmail.com>
2024-09-20 17:52:01 +0300

    Added typing for about page

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -31,4 +31,19 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
+
+  //   Typing About page
+  var typed = new Typed(".typing-2", {
+    strings: [
+      "Software Developer",
+      "Tutor",
+      "Mentor",
+      "Designer",
+      "Blogger",
+      "Freelancer",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
 });
